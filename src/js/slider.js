54360c105a694107f04b7f7f11575dab9862ebee
DaanoontjePower <daandonkers1@gmail.com>
2022-11-16 15:07:16 +0100

    Added Radiobuttons

---
+++ src/js/slider.js
@@ -0,0 +1,36 @@
+
+var arrowleft = document.getElementById("arrow1");
+var arrowright = document.getElementById("arrow2");
+
+arrowleft.onclick = function () {
+    console.log("halloooo");
+}
+
+arrowright.onclick = function () {
+    console.log("halloooo");
+}
+
+
+// switch (images.data - filename) {
+//     case "product-1.png":
+//         console.log("Hello world!");
+//         break;
+//     case "product-2.png":
+//         console.log("Hello world!!");
+//         break;
+//     case "product-3.png":
+//         console.log("Hello world!!!");
+//         break;
+//     case "product-4.png":
+//         console.log("Hello world!!!!");
+//         break;
+//     case "product-5.png":
+//         console.log("Hello world!!!!!");
+//         break;
+//     case "product-6.png":
+//         console.log("Hello world!!!!!!");
+//         break;
+//     case "product-7.png":
+//         console.log("Hello world!!!!!!!");
+//         return;
+// }
